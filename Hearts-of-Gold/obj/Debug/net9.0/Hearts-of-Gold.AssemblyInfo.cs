f227aa35195051540bf5d3a69325bbd04ce4e326
hfayed <hfayed.benzekri@elev.ga.ntig.se>
2026-04-26 18:07:03 +0200

    Update och Diagram

--- Hearts-of-Gold/obj/Debug/net9.0/Hearts-of-Gold.AssemblyInfo.cs
+++ Hearts-of-Gold/obj/Debug/net9.0/Hearts-of-Gold.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hearts-of-Gold")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e60b1d17e10254e835e2c8b0c7c37fc59363c7f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdd463fa14999f2b4714e4b5812eae8920edd654")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hearts-of-Gold")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hearts-of-Gold")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
